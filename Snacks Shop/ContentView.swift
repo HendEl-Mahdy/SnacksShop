adf301652bdc8b24077a6b671c390354e5ac615b
Hend El-Mahdy <hendm8042@gmail.com>
2025-06-25 16:22:14 -0700

    Initial Commit

---
+++ Snacks Shop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snacks Shop
+//
+//  Created by Hend El Mahdy on 25/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snacks Shop/Snacks_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snacks_ShopApp.swift
+//  Snacks Shop
+//
+//  Created by Hend El Mahdy on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Snacks_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
